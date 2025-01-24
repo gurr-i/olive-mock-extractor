be19ca5dd65d687f03fa799e312ff20de7d9f1ae
Gagan <business@devgagan.in>
2025-01-24 23:52:07 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# copyright github.com/devgaganin
+
+from os import getenv
+
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+AUTH_CODE = getenv("AUTH_CODE", "")
