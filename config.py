7e81afdc2c92a8781f1267e85de92ab9781c0989
Gagan <business@devgagan.in>
2025-01-25 00:18:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ from os import getenv
 API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH", "")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-AUTH_CODE = getenv("AUTH_CODE", "")
+AUTH_CODE = getenv("AUTH_CODE", "") # eg: eyfgobtiOtrtrUzI......eyJpc3MiHRwewczovL3Rlc3Rib....
