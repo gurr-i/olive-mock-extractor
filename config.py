1a7e82389c34fe8d57effd977e2978e3b8e70ab2
Gagan <business@devgagan.in>
2025-01-28 11:40:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,5 @@ from os import getenv
 API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH", "")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-AUTH_CODE = getenv("AUTH_CODE", "") # eg: eyfgobtiOtrtrUzI......eyJpc3MiHRwewczovL3Rlc3Rib....
+COOKIES = getenv("AUTH_CODE", "")
+OWNER_ID = getenv("OWNER_ID", "1213121") # if want to make accessible in channel put channel id in owner id field
